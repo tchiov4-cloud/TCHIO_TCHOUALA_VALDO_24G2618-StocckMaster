82361bb0c85a87e6b85d4a180cc174be5f0526aa
Cyberninja <tchiov4@gmail.com>
2026-04-27 15:23:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_db():
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASS'),
         port=int(os.getenv('DB_PORT', 4000)),
-        database=os.getenv('DB_NAME', 'test'),
+        database=os.getenv('DB_NAME', 'valdo_stock'),
         # TRÈS IMPORTANT pour TiDB sur Render :
         ssl_disabled=False,
         ssl_verify_cert=False
